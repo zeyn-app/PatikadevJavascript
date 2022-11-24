72f0b20b8cb7318123673d440758f332b5d18b18
Zeynep Özdemir <zeynepozz2299@gmail.com>
2022-11-24 13:18:54 +0300

    .css file updated

--- project1/clock.js
+++ project1/clock.js
@@ -6,15 +6,18 @@ userName.innerHTML = input;
 
 function showTime(){
     let currentDate = new Date();
-    let dateTime = currentDate.getDate() + ":"
-    + currentDate.getMonth() + ":"
+    let dateTime = currentDate.getDate() + "/"
+    + currentDate.getMonth() + "/"
     + currentDate.getFullYear() + " "
     + (currentDate.getDay.toString() == 1 ? "Pazartesi" : 
     currentDate.getDay() == 2 ? "Salı" : 
     currentDate.getDay() == 3 ? "Çarşamba" : 
     currentDate.getDay() == 4 ? "Perşembe" : 
     currentDate.getDay() == 5 ? "Cuma" : 
-    currentDate.getDay() == 6 ? "Cumartesi" : "Pazar");
+    currentDate.getDay() == 6 ? "Cumartesi" : "Pazar")
+    + currentDate.getHours() + ":"
+    + currentDate.getMinutes() + ":"
+    + currentDate.getSeconds() ;
     time.innerHTML = dateTime + time.innerHTML;
     console.log("<"+currentDate.getDay()+">");
 }
